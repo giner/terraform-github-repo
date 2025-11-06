1ce9ab1253426a947915442aa0b85ae4ba541c8e
Stanislav German-Evtushenko <ginermail@gmail.com>
2025-11-06 11:11:31 +0900

    Update module version in the example

--- examples/github-repo-module-with-custom-defaults/main.tf
+++ examples/github-repo-module-with-custom-defaults/main.tf
@@ -1,5 +1,5 @@
 module "github_repo" {
-  source = "git::https://github.com/giner/terraform-github-repo?ref=f8921369ea3deb3fcc085fc8649f94fd671f9d96" # 1.1.0
+  source = "git::https://github.com/giner/terraform-github-repo?ref=8ec58a0efa2b7b4dcc155f620acccb6045008513" # 1.6.0
 
   organization_default_branch = var.organization_default_branch
 
